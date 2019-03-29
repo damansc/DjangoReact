24e68866e709a4a46ce3ea4286cefc1487229ca6
Josh Wood <wood2jp2@gmail.com>
2019-03-29 11:20:29 -0400

    pushed up a front end but not yet communicating with back end

--- manage.py
+++ manage.py
@@ -2,8 +2,9 @@
 import os
 import sys
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangotesterfiles.settings')
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -11,5 +12,8 @@ if __name__ == '__main__':
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        )
+
+    from configurations.management import execute_from_command_line
+
     execute_from_command_line(sys.argv)
--- wsgi.py
+++ wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangotesterfiles.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangotesterfiles.settings')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
+
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
